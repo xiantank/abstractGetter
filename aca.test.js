4104ef03b42c368af18360ad2721c9d23093f891
xiantank@home <david1992309@gmail.com>
2016-10-19 21:26:07 +0800

    test for slide by sentence/char

--- aca.test.js
+++ aca.test.js
@@ -1,16 +1,30 @@
 "use strict"
 
-var aca = new ACA();
-
-var getter = new AbstractGetter();
-var query = "google apple pixel siri"
-getter.query(query);
-var article = document.body.innerText;
+//var aca = new ACA();
+TestSlideByChar();
+TestSlideBySentence();
+function TestSlideByChar() {
+		var getter = new AbstractGetter();
+		var query = "google apple pixel siri";
+		getter.query(query);
+		var article = document.body.innerText;
 //var start = getAbstractBySlideWindow(a,13);
 //console.log(str.slice(start,start+13));
 
 
-var result = getter.getAbstract(article);
-console.log(query);
-console.log(result);
-
+		var result = getter.getAbstract(article);
+		console.log(query);
+		console.log(result.join("\n\n"));
+}
+function TestSlideBySentence() {
+		var getter = new AbstractGetter({
+				slideType: "sentence",
+				slideSize: 3
+		});
+		var query = "google apple pixel siri";
+		getter.query(query);
+		var article = document.body.innerText;
+		var result = getter.getAbstract(article);
+		console.log(query);
+		console.log(result.join("\n\n"));
+}
